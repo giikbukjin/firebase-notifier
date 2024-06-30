938321d10f550a31fe74d8d0af654a8aaf150580
Jinseo Jeong <js020315@gmail.com>
2024-06-30 15:50:29 +0900

    Project Setting

---
+++ src/main/java/com/example/firebase_notifier/FirebaseNotifierApplication.java
@@ -0,0 +1,13 @@
+package com.example.firebase_notifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirebaseNotifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirebaseNotifierApplication.class, args);
+	}
+
+}
