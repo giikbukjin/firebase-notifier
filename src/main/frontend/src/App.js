49320ea52c6df7f65c1b31169fb100b6c7295e28
Jinseo-Jeong <104565869+giikbukjin@users.noreply.github.com>
2024-07-01 10:23:10 +0900

    [FEAT] Firebase remote config
    React를 이용해 Firebase Remote Config

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,20 +1,42 @@
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
+import './App.css';
+import React, { useEffect } from 'react';
+import Main from "./Components/Main/Main";
+import { initializeApp } from "firebase/app";
+import { activate, fetchAndActivate, fetchConfig, getRemoteConfig, getValue } from "firebase/remote-config";
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
 
 function App() {
-  const [hello, setHello] = useState('')
+    const [greet, setGreet] = React.useState("");
+    const app = initializeApp(firebaseConfig);
+    const remoteConfig = getRemoteConfig(app);
+    remoteConfig.settings.minimumFetchIntervalMillis = 0;
 
-  useEffect(() => {
-    axios.get('/api/hello')
-        .then(response => setHello(response.data))
-        .catch(error => console.log(error))
-  }, []);
+    useEffect(() => {
+        let greeting = '';
+        fetchAndActivate(remoteConfig)
+            .then(() => {
+                greeting = getValue(remoteConfig, 'greet');
+                setGreet(greeting._value);
+            })
+            .catch((err) => {
+                console.log("Failed to fetch remote config", err);
+            });
+    }, []);
 
-  return (
-      <div>
-        백엔드에서 가져온 데이터입니다 : {hello}
-      </div>
-  );
+    return (
+        <>
+            <Main greet={ greet } />
+        </>
+    );
 }
 
 export default App;
