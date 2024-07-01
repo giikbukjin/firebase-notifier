e4392049f700e92bcc38c0f8a2dc3cdea212c34e
Jinseo-Jeong <104565869+giikbukjin@users.noreply.github.com>
2024-07-01 17:16:18 +0900

    [FEAT] Cloud Messaging
    Firebase에서 토큰값 입력해 테스트 메세지 전송
    크롬 웹 알림(우하단)으로 전송됨

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,45 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import { ref, set, onValue } from 'firebase/database';
-import { database } from './firebaseInit';
+import "./firebase-messaging-sw.js";
 
-const App = () => {
-    const [announcements, setAnnouncements] = useState([]);
-
-    useEffect(() => {
-        const announcementsRef = ref(database, 'announcements/');
-        onValue(announcementsRef, (snapshot) => {
-            const data = snapshot.val();
-            if (data) {
-                setAnnouncements(Object.values(data));
-            }
-        });
-    }, []);
-
-    const addAnnouncement = () => {
-        const newAnnouncementRef = ref(database, `announcements/${Date.now()}`);
-        set(newAnnouncementRef, {
-            message: '새로운 공지사항입니다.'
-        })
-            .then(() => {
-                console.log('Announcement added successfully.');
-            })
-            .catch((error) => {
-                console.error('Error adding announcement:', error);
-            });
-    };
-
-    return (
-        <div className="App">
-            <h1>공지사항</h1>
-            <ul>
-                {announcements.map((announcement, index) => (
-                    <li key={index}>{announcement.message}</li>
-                ))}
-            </ul>
-            <button onClick={addAnnouncement}>공지사항 추가</button>
-        </div>
-    );
-};
+function App() {
+    return <h1>Test Firebase Cloud Message</h1>;
+}
 
 export default App;
--- src/main/frontend/src/firebaseInit.js
+++
@@ -1,8 +0,0 @@
-import { initializeApp } from 'firebase/app';
-import firebaseConfig from './firebaseConfig';
-import { getDatabase } from 'firebase/database';
-
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
-
-export { app, database };
